3b83df630ef7dd15ab2076c70025db8609526c9e
Jeremy Leonardo <58095255+jeremy-leonardo@users.noreply.github.com>
2021-02-10 12:57:53 +0700

    add seeders

---
+++ database/seeds/ArticleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('articles')->insert([
+            [
+                'user_id' => 2,
+                'category_id' => 1,
+                'title' => "Pantai Kuta, Bali",
+                'description' => "Pantai Kuta adalah sebuah tempat pariwisata yang terletak kecamatan Kuta, sebelah selatan Kota Denpasar, Bali, Indonesia. Daerah ini merupakan sebuah tujuan wisata turis mancanegara dan telah menjadi objek wisata andalan Pulau Bali sejak awal tahun 1970-an. Pantai Kuta sering pula disebut sebagai pantai matahari terbenam (sunset beach) sebagai lawan dari pantai Sanur. Selain itu, Lapangan Udara I Gusti Ngurah Rai terletak tidak jauh dari Kuta.",
+                'image' => "images/article/324e8619-0b5f-4b9c-af81-c5e3fb236376.jpg",
+            ],
+        ]);
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => "Admin",
+                'email' => "admin@mail.com",
+                'phone' => "08123123",
+                'role' => "admin",
+                'password' => bcrypt("admin123"),
+            ],
+            [
+                'name' => "Anto",
+                'email' => "anto@mail.com",
+                'phone' => "08421421",
+                'role' => "user",
+                'password' => bcrypt("anto123"),
+            ],
+        ]);
+    }
+}
